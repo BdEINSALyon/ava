27bfb479ac8da56bbd3d3228ea5997b6800db9d3
Philippe Vienne <philippe.vienne@insa-lyon.fr>
2016-06-15 00:41:47 +0200

    Add study year to dashboard

---
+++ app/controllers/admin/study_years_controller.rb
@@ -0,0 +1,19 @@
+module Admin
+  class StudyYearsController < Admin::ApplicationController
+    # To customize the behavior of this controller,
+    # simply overwrite any of the RESTful actions. For example:
+    #
+    # def index
+    #   super
+    #   @resources = StudyYear.all.paginate(10, params[:page])
+    # end
+
+    # Define a custom finder by overriding the `find_resource` method:
+    # def find_resource(param)
+    #   StudyYear.find_by!(slug: param)
+    # end
+
+    # See https://administrate-docs.herokuapp.com/customizing_controller_actions
+    # for more information
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :admin do
     resources :users
+    resources :study_years
     root to: 'users#index'
   end
 
