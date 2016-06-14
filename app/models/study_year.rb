e70354687d6d919881df9de7cc453cd557059a62
Philippe Vienne <philippe.vienne@insa-lyon.fr>
2016-06-15 00:45:15 +0200

    Add condition to consider study year as valid

--- app/models/study_year.rb
+++ app/models/study_year.rb
@@ -3,5 +3,6 @@ class StudyYear < ActiveRecord::Base
   validates_presence_of :name, :year
   validates_length_of :name, minimum: 1, maximum: 64
   validates_inclusion_of :year, in: 0..12
+  validates_numericality_of :year
 
 end
