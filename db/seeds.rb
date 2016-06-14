c6d34d65b039cdd1ae7abd924b42d48a4ad28a10
Philippe Vienne <philippe.vienne@insa-lyon.fr>
2016-06-15 00:50:34 +0200

    Add minimal seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# WARNING: This seed file was designed for BdE INSA Lyon, and can not be suitable for your Union
+
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+(1...5).each do |year|
+  StudyYear.find_or_create_by! year: year, name: "#{year}A"
+end
+StudyYear.find_or_create_by! year: 6, name: 'Doctorat'
+StudyYear.find_or_create_by! year: 10, name: 'Autre'
+
+puts 'Created Study Years'
+
+%w(BB BIM GCU GE GEN GI GM IF SGM TC).each do |dep|
+  Department.find_or_create_by! name: dep
+end
+
+puts 'Created Departments'
