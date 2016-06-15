0f1a9e0fa1aa6bebf8207f3f595bf5b12df6e6c1
Philippe Vienne <philippe.vienne@insa-lyon.fr>
2016-06-15 02:28:37 +0200

    Improved show member page

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -1,2 +1,10 @@
 module MembersHelper
+  # @param [Department] department
+  def display_department(department)
+    department.nil? ? t('not_filled') : department.name
+  end
+  # @param [StudyYear] sy
+  def display_study_year(sy)
+    sy.nil? ? t('other') : sy.name
+  end
 end
